745e6e101a3c9e2a4102b64c91f937e3b66d86fe
Reck <gsdt008@gmail.com>
2023-07-22 16:24:19 +0545

    Fixed chart component for no data

--- frontend/components/ChartComponent.js
+++ frontend/components/ChartComponent.js
@@ -7,12 +7,21 @@ function ChartComponent({data}) {
     title: "Monthly Expenses",
     chartArea: { width: "80%", height: "70%" },
   };
+  let arrayOfData = [
+    ["Name", "Amount"],
+      ];
+
+  data.forEach((item) => arrayOfData.push([item.title,item.amount]));
+  const defaulData = [
+    ["Name", "Amount"],
+    ["Add something to see...", 0],
+  ]
   return (
     <Chart
     chartType="AreaChart"
     width="100%"
     height="300px"
-    data={data}
+    data={arrayOfData.length > 1 ? arrayOfData : defaulData}
     options={options}
   />
           );
--- frontend/components/MinMax.js
+++ frontend/components/MinMax.js
@@ -4,7 +4,6 @@ function MinMax({incomes, expenses}) {
 
     let sortedIncome = incomes.sort((a,b) => a.amount-b.amount);
     let sortedExpense = expenses.sort((a,b) => a.amount-b.amount);
-  
 
   return (
     <table className='p-3  w-[85vw] md:w-[50vw] lg:w-[60vw] xl:w-[30vw] rounded space-y-3'>
@@ -16,9 +15,9 @@ function MinMax({incomes, expenses}) {
   <th>Max</th>
       </tr>
   <tr className='text-center bg-white h-12 md:h-14 w-[25vw] rounded'>
-   <td className='text-green-500'>{sortedIncome && sortedIncome[0]?.amount}</td>
+   <td className='text-green-500'>{sortedIncome.length > 0 ? sortedIncome[0]?.amount : 0}</td>
    <td>|</td>
-   <td className='text-green-500'>{sortedIncome && sortedIncome[sortedIncome.length-1]?.amount}</td>
+   <td className='text-green-500'>{sortedIncome.length > 0 ? sortedIncome[sortedIncome.length-1]?.amount : 0}</td>
 </tr>
 <tr>
   <th>Min</th>
@@ -26,9 +25,9 @@ function MinMax({incomes, expenses}) {
   <th>Max</th>
       </tr>
       <tr className='text-center bg-white h-12 md:h-14 w-[25vw] rounded'>
-      <td className='text-red-500'>{sortedExpense && sortedExpense[0]?.amount}</td>
+      <td className='text-red-500'>{sortedExpense.length > 0 ? sortedExpense[0]?.amount : 0}</td>
       <td>|</td>
-      <td className='text-red-500'>{sortedExpense && sortedExpense[sortedExpense.length-1]?.amount}</td>
+      <td className='text-red-500'>{sortedExpense.length > 0 ? sortedExpense[sortedExpense.length-1]?.amount : 0}</td>
 </tr>
     </tbody>
 </table>
